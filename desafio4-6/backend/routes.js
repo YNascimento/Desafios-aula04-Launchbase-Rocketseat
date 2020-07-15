effce1affff464fc2a4049bd288be34966a84cdc
YSNascimento <yuri.nascimento.ro@gmail.com>
2020-07-15 11:59:59 -0300

    desafio 4-6 feito

---
+++ desafio4-6/backend/routes.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const routes = express.Router()
+const teacher = require('./teacher')
+
+//home
+routes.get('/',function(req,res){
+    return res.redirect('./teachers')
+})
+
+//index Teachers
+routes.get('/teachers', teacher.index)
+
+//Create Teacher
+routes.get('/teachers/create',function(req,res){
+    return res.render('teachers/create')
+})
+
+//POST back from create
+routes.post('/teachers', teacher.validatePost)
+
+//edit teacher info
+routes.get('/teachers/:id/edit', teacher.edit)
+
+//Show Teacher
+routes.get('/teachers/:id', teacher.show)
+
+routes.put('/teachers', teacher.put)
+
+
+routes.delete('/teachers', teacher.delete)
+
+//index Students
+routes.get('students', function(req,res){
+    return res.render('students/index')
+})
+
+module.exports = routes

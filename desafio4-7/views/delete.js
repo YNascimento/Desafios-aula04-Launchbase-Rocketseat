fea2c1e8565ac661f6f94c3a873f2175cbcbc3bf
YSNascimento <yuri.nascimento.ro@gmail.com>
2020-07-16 13:24:31 -0300

    desafio 4-7 feito

---
+++ desafio4-7/views/delete.js
@@ -0,0 +1,8 @@
+const formDelete = document.getElementById('form-delete')
+console.log(formDelete)
+formDelete.addEventListener('submit',function(event){
+    const confirmation = confirm("Tem certeza?")
+    if(!confirmation){
+        event.preventDefault()
+    }
+})
